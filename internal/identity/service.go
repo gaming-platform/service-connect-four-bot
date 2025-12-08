583ba430058c4fcc44dd5cbdd4fffa7697adf94c
Markus Reinhold <markusreinhold@icloud.com>
2025-12-08 12:44:07 +0100

    Use generated constants for message names

--- internal/identity/service.go
+++ internal/identity/service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 
+	commonv1 "github.com/gaming-platform/api/go/common/v1"
 	identityv1 "github.com/gaming-platform/api/go/identity/v1"
 	"github.com/gaming-platform/connect-four-bot/internal/api"
 	"github.com/gaming-platform/connect-four-bot/internal/rpcclient"
@@ -27,13 +28,13 @@ func (s *BotService) RegisterBot(ctx context.Context, username string) (string, 
 		return "", err
 	}
 
-	resp, err := s.rpcClient.Call(ctx, rpcclient.Message{Name: "Identity.RegisterBot", Body: reqBody})
+	resp, err := s.rpcClient.Call(ctx, rpcclient.Message{Name: identityv1.RegisterBotType, Body: reqBody})
 	if err != nil {
 		return "", err
 	}
 
 	switch resp.Name {
-	case "Identity.RegisterBotResponse":
+	case identityv1.RegisterBotResponseType:
 		var regBotResp identityv1.RegisterBotResponse
 		err = proto.Unmarshal(resp.Body, &regBotResp)
 		if err != nil {
@@ -41,7 +42,7 @@ func (s *BotService) RegisterBot(ctx context.Context, username string) (string, 
 		}
 
 		return regBotResp.BotId, nil
-	case "Common.ErrorResponse":
+	case commonv1.ErrorResponseType:
 		return "", api.ErrorResponseToError(resp.Body)
 	default:
 		return "", errors.New("unknown response")
@@ -55,13 +56,13 @@ func (s *BotService) GetBotByUsername(ctx context.Context, username string) (*Bo
 		return nil, err
 	}
 
-	resp, err := s.rpcClient.Call(ctx, rpcclient.Message{Name: "Identity.GetBotByUsername", Body: reqBody})
+	resp, err := s.rpcClient.Call(ctx, rpcclient.Message{Name: identityv1.GetBotByUsernameType, Body: reqBody})
 	if err != nil {
 		return nil, err
 	}
 
 	switch resp.Name {
-	case "Identity.GetBotByUsernameResponse":
+	case identityv1.GetBotByUsernameResponseType:
 		var getBotResp identityv1.GetBotByUsernameResponse
 		err = proto.Unmarshal(resp.Body, &getBotResp)
 		if err != nil {
@@ -69,7 +70,7 @@ func (s *BotService) GetBotByUsername(ctx context.Context, username string) (*Bo
 		}
 
 		return fromProtoBot(getBotResp.Bot), nil
-	case "Common.ErrorResponse":
+	case commonv1.ErrorResponseType:
 		return nil, api.ErrorResponseToError(resp.Body)
 	default:
 		return nil, errors.New("unknown response")
