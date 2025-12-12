28e5e3d95c0d8f9a3b4ef272a29aecbbc63c6c50
Markus Reinhold <markusreinhold@icloud.com>
2025-12-12 20:11:08 +0100

    Write chat messages

--- internal/bot/opening.go
+++ internal/bot/opening.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 
 	connectfourv1 "github.com/gaming-platform/api/go/connectfour/v1"
+	"github.com/gaming-platform/connect-four-bot/internal/chat"
 	"github.com/gaming-platform/connect-four-bot/internal/connectfour"
 	"github.com/gaming-platform/connect-four-bot/internal/sse"
 )
@@ -12,18 +13,21 @@ import (
 type OpeningBot struct {
 	botId       string
 	sseClient   *sse.Client
+	chatService *chat.ChatService
 	gameService *connectfour.GameService
 }
 
-func NewOpeningBot(botId string, client *sse.Client, cfSvc *connectfour.GameService) *OpeningBot {
-	return &OpeningBot{
-		botId:       botId,
-		sseClient:   client,
-		gameService: cfSvc,
-	}
+func NewOpeningBot(
+	botId string,
+	client *sse.Client,
+	chatSvc *chat.ChatService,
+	gameSvc *connectfour.GameService,
+) *OpeningBot {
+	return &OpeningBot{botId: botId, sseClient: client, chatService: chatSvc, gameService: gameSvc}
 }
 
 func (b *OpeningBot) Play(ctx context.Context) error {
+	var chatId string
 	width := 7
 	gameId, err := b.getGameIdForPlay(ctx, width)
 	if err != nil {
@@ -44,6 +48,20 @@ func (b *OpeningBot) Play(ctx context.Context) error {
 			}
 
 			switch res.Event.Name {
+			case "ConnectFour.ChatAssigned":
+				v, ok := res.Event.Payload["chatId"].(string)
+				if !ok {
+					continue
+				}
+				chatId = v
+
+				go b.chatService.WriteMessage(
+					ctx,
+					chatId,
+					b.botId,
+					"Good luck, have fun!",
+					"opening",
+				)
 			case "ConnectFour.PlayerJoined":
 				redPlayerId, ok := res.Event.Payload["redPlayerId"].(string)
 				if !ok || redPlayerId != b.botId {
@@ -70,11 +88,32 @@ func (b *OpeningBot) Play(ctx context.Context) error {
 						break
 					}
 				}
-			case "ConnectFour.GameAborted",
-				"ConnectFour.GameWon",
+			case "ConnectFour.GameAborted":
+				if chatId == "" {
+					continue
+				}
+				go b.chatService.WriteMessage(
+					ctx,
+					chatId,
+					b.botId,
+					"Next time, perhaps!",
+					"ending",
+				)
+				sseCancel()
+			case "ConnectFour.GameWon",
 				"ConnectFour.GameDrawn",
 				"ConnectFour.GameTimedOut",
 				"ConnectFour.GameResigned":
+				if chatId == "" {
+					continue
+				}
+				go b.chatService.WriteMessage(
+					ctx,
+					chatId,
+					b.botId,
+					"Good game! Well played.",
+					"ending",
+				)
 				sseCancel()
 			}
 		}
